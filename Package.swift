943488c586cffaf318f4cc1dc4f50972b3cc3f2f
Andrei Panov <panovdev@gmail.com>
2026-05-14 21:07:23 +0200

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,11 +5,38 @@ import PackageDescription
 
 let package = Package(
     name: "LocalHarness",
+    platforms: [
+        .macOS(.v14)
+        ],
+    products: [
+        .library(
+            name: "HarnessCore",
+            targets: ["HarnessCore"]
+        ),
+        .executable(
+            name: "harness",
+            targets: ["HarnessCLI"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/apple/swift-argument-parser",
+            from: "1.7.0"
+        )
+        ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "HarnessCore"
+        ),
         .executableTarget(
-            name: "LocalHarness"
+            name: "HarnessCLI",
+            dependencies: [
+                "HarnessCore",
+                .product(
+                    name: "ArgumentParser",
+                    package: "swift-argument-parser"
+                )
+            ]
         ),
         .testTarget(
             name: "LocalHarnessTests",
